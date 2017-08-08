1ac8a60817b7fa21f3e2cf1f8fe2eb73d142e6b5
Chris Nelson <superchrisnelson@gmail.com>
2017-08-08 17:09:25 -0400

    Needed this to avoid error calling the rust function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const wasm = require('./main.rs')
 
-wasm.initialize().then(module => {
+wasm.initialize({noExitRuntime: true}).then(module => {
   const add = module.cwrap('add', 'number', ['number', 'number'])
   console.log('Calling rust functions from javascript!')
   console.log(add(1, 2))
